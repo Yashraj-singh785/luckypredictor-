6d4b2351f57faab2199db2a039fbc90ccdb645c2
Yashraj-singh785 <yashrajsingh7851@gmail.com>
2026-05-06 10:39:51 +0530

    Deleting lucky.py.py bec. of extension

--- lucky.py
+++ lucky.py
@@ -1,18 +1,28 @@
-import sys,random
-print ("welcome to the Lucky predictor 'Funny name picker .'\n")
-print("A name  which is more funnier and creative :\n\n")
+"""Generate funny names by randomly combining names from 2 seprate lists."""
+import sys
+import random
 
-first = ('Shiva', 'Divyansh' , 'Dishant' , 'Ayush' , 'Hardik' ,)
-last = ('Yadav','Shukla','Prajapati','Trigger','Shekawat')
+def main():
+    """Choose names at random from 2 tuples of names and print to scren ."""
+    print ("welcome to the Lucky predictor 'Funny name picker .'\n")
+    print("A name  which is more funnier and creative :\n\n")
 
-while True :
-    firstName = random.choice(first)
-    lastName = random.choice(last)
-    print("\n\n")
-    print("{}{}".format(firstName, lastName) ,file =sys.stderr)
-    print("\n\n")
+    first = ('Shiva', 'Divyansh' , 'Dishant' , 'Ayush' , 'Hardik' ,)
+    last = ('Yadav','Shukla','Prajapati','Trigger','Shekawat')
 
-    try_again =input("\n\nTry again?(Press Enter else n to quit)\n ")
-    if try_again.lower() == "n":
-        break
+    while True :
+        first_Name = random.choice(first)
+        last_Name = random.choice(last)
+        print("\n\n")
+        # Trick IDLE by using "fatal error"setting to print name in red .
+        print("{} {}".format(first_Name, last_Name) ,file =sys.stderr)
+        print("\n\n")
+
+        try_again =input("\n\nTry again?(Press Enter else n to quit)\n ")
+        # converting every N TO n 
+        if try_again.lower() == "n":
+            break
+    
     input("\nPress Enter to exit.")
+      
+
